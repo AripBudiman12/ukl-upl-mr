9d889031fa7e526124932402076d1afcca727c9c
Aldo Bramantio Nugroho <aldobrahmantyo321@gmail.com>
2024-11-15 09:02:16 +0700

    Update Redis Session

--- config/database.php
+++ config/database.php
@@ -172,6 +172,14 @@ return [
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
+        'session' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_SESSION_DB', '2'),
+            'prefix' => 'amdalnet_session_',  // Set specifically for session
+        ],
     ],
 
 ];
